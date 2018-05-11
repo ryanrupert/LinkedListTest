a402de48426aa732fe9da31e8787f86c5be2535c
Ryan Rupert <mudge25+code@gmail.com>
2018-05-11 12:23:32 -0700

    Add list.h
    This is moving the linked list code to a class.

---
+++ List.h
@@ -0,0 +1,28 @@
+/**
+* Created by Ryan Rupert
+* Date: 2018-05-11
+* Source: None
+* This is the list class to test some linked list stuff.
+*/
+#include <iostream>
+
+class List
+{
+	private:
+		struct Node
+		{
+			int data;
+			struct Node *next;
+			struct Node *prev;
+		};
+		Node *head;
+		Node *tail;
+
+	public:
+		List();
+		~List();
+		void insertEnd(int d);
+		void deleteEnd();
+		void deleteBegin();
+		friend std::ostream& operator<<(std::ostream& os, const List& list);
+};
