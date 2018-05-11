930cc7514f1ef3d3172b10e549e18709eaf4498f
Ryan Rupert <mudge25+code@gmail.com>
2018-05-11 13:36:44 -0700

    Format Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,21 +4,21 @@
 int main()
 {
 	List list;
-  //insert 1 at the end of the list
-  list.insertEnd(1);
-  //insert 2 at the end of the list
-  list.insertEnd(2);
-  //insert 4 at the end of the list
-  list.insertEnd(4);
-  //insert 3 at the end of the list
-  list.insertEnd(3);
-  //insert 6 at the end of the list
-  list.insertEnd(6);
-  //insert 5 at the end of the list
-  list.insertEnd(5);
-  std::cout << list << std::endl;
+	//insert 1 at the end of the list
+	list.insertEnd(1);
+	//insert 2 at the end of the list
+	list.insertEnd(2);
+	//insert 4 at the end of the list
+	list.insertEnd(4);
+	//insert 3 at the end of the list
+	list.insertEnd(3);
+	//insert 6 at the end of the list
+	list.insertEnd(6);
+	//insert 5 at the end of the list
+	list.insertEnd(5);
+	std::cout << list << std::endl;
 	std::cout << list[1] << std::endl;
-  std::cin.get();
+	std::cin.get();
 
-  return 0;
+	return 0;
 }
