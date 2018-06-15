143beeee70179c552147389ac9954366dcb840dd
Ryan Rupert <mudge25+code@gmail.com>
2018-06-15 11:49:52 -0700

    Fix insert method call in Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,17 +5,18 @@ int main()
 {
 	List list;
 	//insert 1 at the end of the list
-	list.insertEnd(1);
+	list.insert(1);
 	//insert 2 at the end of the list
-	list.insertEnd(2);
+	list.insert(2);
 	//insert 4 at the end of the list
-	list.insertEnd(4);
+	list.insert(4);
 	//insert 3 at the end of the list
-	list.insertEnd(3);
+	list.insert(3);
 	//insert 6 at the end of the list
-	list.insertEnd(6);
+	list.insert(6);
 	//insert 5 at the end of the list
-	list.insertEnd(5);
+	list.insert(5);
+	list.insert(20, 0);
 	std::cout << list << std::endl;
 	std::cout << list[1] << std::endl;
 	list[2] = 7;
